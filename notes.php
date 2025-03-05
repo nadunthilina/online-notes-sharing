839bdbd22e7e3cfa632ad8fe699d4029caba3a18
nadun <nadunthilina13@gmail.com>
2025-03-05 11:02:30 +0530

    adding note php

---
+++ notes.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+error_reporting(0);
+include('user/includes/dbconnection.php');
+?>
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+    <title>Online Notes Sharing System | Notes</title>
+   
+    <link rel="manifest" href="site.webmanifest">
+        <!-- CSS here -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/progressbar_barfiller.css">
+    <link rel="stylesheet" href="assets/css/gijgo.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/animated-headline.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
