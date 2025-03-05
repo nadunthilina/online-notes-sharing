698359e57c3ea4ee7b01c96c43c19570b8db72f0
nadun <nadunthilina13@gmail.com>
2025-03-05 10:55:41 +0530

    update 1

--- index.php
+++ index.php
@@ -22,3 +22,19 @@
     <link rel="stylesheet" href="assets/css/style.css">
     
 </head>
+<body>
+  <?php include_once('includes/header.php');?>
+    <main>
+        <!--? slider Area Start-->
+        <section class="slider-area ">
+            <div class="slider-active">
+                <!-- Single Slider -->
+                <div class="single-slider slider-height d-flex align-items-center">
+                    <div class="container">
+                        <div class="row"></div>
+                        <div class="col-xl-6 col-lg-7 col-md-12">
+                                <div class="hero__caption">
+                                    <h1 data-animation="fadeInLeft" data-delay="0.2s">Online learning<br> platform</h1>
+                                    <p data-animation="fadeInLeft" data-delay="0.4s">Share Your Notes Online</p>
+                                    <a href="user/signup.php" class="btn hero-btn" data-animation="fadeInLeft" data-delay="0.7s">Join for Free</a>
+                                </div>
